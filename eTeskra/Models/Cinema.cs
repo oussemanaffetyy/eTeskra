beabf6942c84f077fb652e7e13906dbb992e7141
Oussema Naffety <oussemanaffety@sfax.r-iset.tn>
2023-12-24 00:19:57 +0100

    Add Controller + Views (Index)

--- eTeskra/Models/Cinema.cs
+++ eTeskra/Models/Cinema.cs
@@ -6,9 +6,12 @@ namespace eTeskra.Models
     {
         [Key]
         public int Id { get; set; }
-        public string Logo { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+		[Display(Name = "Cinema Logo")]
+		public string Logo { get; set; }
+		[Display(Name = "Name")]
+		public string Name { get; set; }
+		[Display(Name = "Description")]
+		public string Description { get; set; }
 
         //Relationship
         public List<Movie> Movies { get; set; }
--- eTeskra/Models/Producer.cs
+++ eTeskra/Models/Producer.cs
@@ -6,9 +6,12 @@ namespace eTeskra.Models
     {
         [Key]
         public int Id { get; set; }
-        public string ProfilePictureURL { get; set; }
-        public string FullName { get; set; }
-        public string Bio { get; set; }
+		[Display(Name = "Profile Picture")]
+		public string ProfilePictureURL { get; set; }
+		[Display(Name = "Full Name")]
+		public string FullName { get; set; }
+		[Display(Name = "Biography")]
+		public string Bio { get; set; }
 
         //Relationship
         public List<Movie> Movies { get; set; }
