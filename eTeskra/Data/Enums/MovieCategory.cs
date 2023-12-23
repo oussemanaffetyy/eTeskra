018baf374c7fcdb19391202e3f53f95a08254a71
Oussema Naffety <oussemanaffety@sfax.r-iset.tn>
2023-12-23 13:52:16 +0100

    Ajoutez des fichiers projet.

---
+++ eTeskra/Data/Enums/MovieCategory.cs
@@ -0,0 +1,20 @@
+﻿namespace eTeskra.Data.Enums
+{
+    public enum MovieCategory
+    {
+        Action = 1,
+        Fantasy,
+        Drama,
+        Cartoon,
+        Horror,
+        Science_Fiction,
+        Mystery,
+        Thriller,
+        Romance,
+        Animation,
+        Documentary,
+        Family,
+        Biography,
+
+    }
+}
---
+++ eTeskra/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eTeskra.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
