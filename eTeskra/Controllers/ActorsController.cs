03e4b4097188df338599a68b66549b20b125f733
Oussema Naffety <oussemanaffety@sfax.r-iset.tn>
2023-12-25 13:02:09 +0100

    Details Actor

--- eTeskra/Controllers/ActorsController.cs
+++ eTeskra/Controllers/ActorsController.cs
@@ -16,7 +16,7 @@ namespace eTeskra.Controllers
         }
 		public async Task<IActionResult> Index()
 		{
-			var data = await _service.GetAll();
+			var data = await _service.GetAllAsync();
 			return View(data);
 		}
 
@@ -33,9 +33,18 @@ namespace eTeskra.Controllers
 				return View(actor);
 			}
 
-			_service.Add(actor);
+			await _service.AddAsync(actor);
 			return RedirectToAction(nameof(Index));
 		}
 
-	}
+        //Get: Actors/Details
+        public async Task<IActionResult> Details(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+            if (actorDetails==null)   return View("Empty");
+
+            return View(actorDetails);
+        }
+
+    }
 }
